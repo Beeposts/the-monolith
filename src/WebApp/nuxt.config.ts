93c28ab9f5aba3d3ab6454f42bd0214fdef57781
Hederson Boechat <hedersonboechat@gmail.com>
2024-08-02 17:33:34 -0300

    chore: use local certs for https dev

--- src/WebApp/nuxt.config.ts
+++ src/WebApp/nuxt.config.ts
@@ -1,57 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 import { defineNuxtConfig } from 'nuxt/config'
-import { spawn } from "child_process";
-import fs from "fs";
-import path from "path";
-import * as process from "process";
-
-const certificateName = process.env.npm_package_name;
-
-// Get base folder for certificates.
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
-
-// Define certificate filepath
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-// Define key filepath
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-export default defineNuxtConfig(async ()=> {
-  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    // Wait for the certificate to be generated
-    await new Promise<void>((resolve) => {
-      spawn('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-      ], { stdio: 'inherit', })
-          .on('exit', (code: any) => {
-            resolve();
-            if (code) {
-              process.exit(code);
-            }
-          });
-    });
-  }
-  
-  const config = {
-    compatibilityDate: '2024-04-03',
-    sourceMap: true,
-    devtools: {enabled: true},
-    devServer:{
-      https:{
-        cert: certFilePath,
-        key: keyFilePath
-      }
-    },
-    modules: ['@nuxt/eslint']
-  }
-  return config;
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  sourcemap: true,
+  devtools: {enabled: true},
+  devServer:{
+    https:{
+      cert: 'localhost.pem',
+      key: 'localhost-key.pem'
+    }
+  },
+  modules: ['@nuxt/eslint', '']
 })
